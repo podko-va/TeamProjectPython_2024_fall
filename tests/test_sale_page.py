a2cbcb8f77c5b749b94d43d897fb0493765c1ed8
Mila Arh <rover321@mail.ru>
2024-04-29 23:24:22 +0400

    TC_011.004.04 | Sale > GEAR DEALS on side panel > Visibility of name GEAR DEALS (#6)

--- tests/test_sale_page.py
+++ tests/test_sale_page.py
@@ -9,3 +9,8 @@ def test_011_001_001_sale_breadcrumbs_is_correct():
     browser.open(SalePageLocators.LINK_SALE)
     ss(BaseLocators.BREADCRUMBS_LIST).should(have.texts('Home', 'Sale'))
 
+
+@allure.link('https://trello.com/c/6x8wE9U7')
+def test_availability_of_name():
+    browser.open('https://magento.softwaretestingboard.com/sale.html')
+    browser.element(SalePageLocators.GEAR_DEALS_TITLE).should(be.visible)
