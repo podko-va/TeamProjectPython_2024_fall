7fd5724b827925be8069e34c1925c4d1aa4e40b9
aqaestuser <144237758+aqaestuser@users.noreply.github.com>
2024-05-01 01:52:22 +0300

    TC_004.005.005 | Sign in & Registration, Account >Sign in_(authorization) > Sign-in error handling & redirection (#31)

--- tests/test_sale_women_sale.py
+++ tests/test_sale_women_sale.py
@@ -4,7 +4,8 @@ from selene import browser, be, have
 from selene.support.shared.jquery_style import s, ss
 import allure
 
-@pytest.mark.xfail
+
+@pytest.mark.skip
 @allure.link('https://trello.com/c/Ae5Bscv3')
 def test_011_011_001_women_sale_breadcrumbs_is_correct():
     # assert error !!! 'Sale' is missing
--- tests/test_sign_in.py
+++ tests/test_sign_in.py
@@ -10,3 +10,10 @@ def test_sign_in_with_good_credentials():
     sign_in.login("pamela341714226113@example.com", "@8j%Yltt(E")
     my_account.page_title("My Account")
 
+
+@allure.link("https://trello.com/c/L5xi1X8i")
+@allure.feature("Sign in & Registration, Account >Sign in_(authorization)")
+def test_sign_in_with_bad_credentials():
+    sign_in.visit()
+    sign_in.login("jasonbrown1714146903@example.net", "wrong_password")
+    message.should_be("account sign-in was incorrect")
