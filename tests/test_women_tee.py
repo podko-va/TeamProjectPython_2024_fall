3080d88d39eea7d949f0b39ac6685b6e0aa4dc09
Cruz06 <Olarut72@gmail.com>
2024-05-01 09:52:04 +0300

    TC_011.016.002 | Sale > Tees-women > Breadcrumbs redirects to the correct pages

--- tests/test_women_tee.py
+++ tests/test_women_tee.py
@@ -9,3 +9,13 @@ def test_011_016_001_women_tees_breadcrumbs_is_correct():
     browser.open(SalePageLocators.LINK_TEES_WOMEN)
     ss(BaseLocators.BREADCRUMBS_LIST).should(have.texts('Home', 'Women', 'Tops', 'Tees'))
 
+
+@allure.link('https://trello.com/c/B29UMcGd')
+def test_011_016_002_breadcrumbs_redirection_from_women_tees():
+    browser.open(SalePageLocators.LINK_TEES_WOMEN)
+    elements = ss(BaseLocators.BREADCRUMBS_LINKS).by(have.attribute('href'))
+    expected_links = ['https://magento.softwaretestingboard.com/',
+                      'https://magento.softwaretestingboard.com/women.html',
+                      'https://magento.softwaretestingboard.com/women/tops-women.html']
+    for i, element in enumerate(elements):
+        element.should(have.attribute('href').value(expected_links[i]))
