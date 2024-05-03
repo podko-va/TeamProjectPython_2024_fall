67ce35f22af9726dc9c630e1022bd39d273b4574
Vlad Guliakin <vladislav.gulyakin@gmail.com>
2024-05-03 14:12:25 -0400

    Add the allure steps

--- tests/test_copyright_information_visibility.py
+++ tests/test_copyright_information_visibility.py
@@ -1,4 +1,5 @@
 import pytest
+import allure
 import data.links as l
 from pages.copyright_info import scroll_to, is_copyright_info_visible
 from selene import browser
@@ -13,9 +14,15 @@ links = [l.BASE_URL,
 
 FOOTER = "small[class='copyright']"
 
+@allure.link("https://trello.com/c/AmBznN3p/160-tc012002001-footer-copyright-information-visibility")
+@allure.feature("Footer > Self > Copyright>visibility")
+@allure.title("TC_012.002.001 | Footer > Copyright information visibility")
 @pytest.mark.parametrize('links', links)
 def test_copyright_information_visibility(links):
-    browser.open(links)
-    scroll_to(browser)
-    is_copyright_info_visible(FOOTER)
+    with allure.step("Navigate to the home page"):
+        browser.open(links)
+    with allure.step("Scroll down to the footer section"):
+        scroll_to(browser)
+    with allure.step("Find the copyright information"):
+        is_copyright_info_visible(FOOTER)
 
