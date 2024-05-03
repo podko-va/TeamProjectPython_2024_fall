2ec21cd7c5605b1b4f295e29b78ab724ac846636
Liliana <lilianakoric@gmail.com>
2024-05-02 22:24:15 -0700

    TC_005.001.001| Main page > Cart > Check Click on the cart icon a window with a blue link View and edit cart should appear

--- tests/test_cart_icon_view_and_edit_cartLK.py
+++ tests/test_cart_icon_view_and_edit_cartLK.py
@@ -1,8 +1,6 @@
 from pages import redirect_inside_cart_LK
 
 
-
-
 def test_cart_icon_view_and_edit_cart():
     redirect_inside_cart_LK.open_main_page()
     redirect_inside_cart_LK.click_title_item()
@@ -11,3 +9,4 @@ def test_cart_icon_view_and_edit_cart():
     redirect_inside_cart_LK.click_add_to_cart_button()
     redirect_inside_cart_LK.click_cart_icon2()
     redirect_inside_cart_LK.assert_view_and_edit_cart_blue_text_visibility()
+
