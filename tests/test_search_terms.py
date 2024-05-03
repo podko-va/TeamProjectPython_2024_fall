c53e6a21b9d9608df2fe5c8566b060a47d3e9ea3
Cruz06 <Olarut72@gmail.com>
2024-05-03 23:16:34 +0300

    TC_015.001.003 | Popular Search Terms > Search terms in the list have sizes, ranging from 76% to 136%

--- tests/test_search_terms.py
+++ tests/test_search_terms.py
@@ -1,6 +1,6 @@
 from pages.locators import SearchTermsLocators as ST
 from pages.locators import BaseLocators as Base
-from selene import browser, be, have
+from selene import browser, be, have, query
 from selene.support.shared.jquery_style import s, ss
 import allure
 
@@ -16,3 +16,15 @@ def test_015_001_002_count_search_terms():
     browser.open(ST.LINK_SEARCH_TERMS)
     ss(ST.TERMS_FOR_SEARCH_LIST_QTY).should(have.size(100))
 
+
+@allure.link('https://trello.com/c/VwsOnXB6')
+def test_015_001_003_check_if_search_terms_has_size_from_76_till_136():
+    # assert from selenium - how to check sizes
+    browser.open(ST.LINK_SEARCH_TERMS)
+    list_font_sizes = []
+    terms = ss(ST.LIST_OF_SEARCH_TERMS)
+    for g in terms:
+        g_font, g_size = g.get(query.attribute("style")).split(": ")
+        g_size = float(g_size.replace("%;", ""))
+        list_font_sizes.append(g_size)
+    assert min(list_font_sizes) <= 76 and max(list_font_sizes) >= 136, "Font sizes not between 76 and 136"
