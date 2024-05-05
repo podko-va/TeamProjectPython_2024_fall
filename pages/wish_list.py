14af32a0c9fb10142b8fc1bff432452f56623408
Vladislav <63920618+vguliakin@users.noreply.github.com>
2024-05-05 08:54:19 -0400

    Update wish_list.py
    Removed '__' of variables, Fixed check for deleting an item from the list

--- pages/wish_list.py
+++ pages/wish_list.py
@@ -1,13 +1,16 @@
 from selene import browser, be, have
 from selene.support.shared.jquery_style import s, ss
+from selenium.webdriver.common.by import By
+
 
 url = "https://magento.softwaretestingboard.com/wishlist/"
-__product_url = "https://magento.softwaretestingboard.com/aether-gym-pant.html?qty=1#143=&93="
+product_url = "https://magento.softwaretestingboard.com/aether-gym-pant.html?qty=1#143=&93="
 
-__ADD_TO_WISHLIST = "a[class='action towishlist']"
-__SUCCESS_MSG = "div[class='message-success success message']"
-__ITEM_CARD = "(//div[@class='product-item-info'])[1]"
-__REMOVE_ITEM = "//div[@class='product-item-actions']/a[@class='btn-remove action delete']"
+ADD_TO_WISHLIST = "a[class='action towishlist']"
+SUCCESS_MSG = "div[class='message-success success message']"
+ITEM_CARD = "(//div[@class='product-item-info'])[1]"
+REMOVE_ITEM = "//div[@class='product-item-actions']/a[@class='btn-remove action delete']"
+ITEM_TITLE = "(//div[@class='products-grid wishlist']//a[@class='product-item-link'])[1]"
 
 
 def visit(url=url):
@@ -26,15 +29,17 @@ def url_should_contain(param):
     browser.should(have.url_containing(param))
 
 def remove_item():
-    s(__REMOVE_ITEM).should(be.visible).click()
+    s(REMOVE_ITEM).should(be.visible).click()
 
 def add_item_to_wish_list():
-    visit(__product_url)
-    s(__ADD_TO_WISHLIST).should(be.clickable).click()
-    s(__SUCCESS_MSG).should(be.visible)
+    visit(product_url)
+    s(ADD_TO_WISHLIST).should(be.clickable).click()
+    s(SUCCESS_MSG).should(be.visible)
 
 def hover_over_item():
-    s(__ITEM_CARD).should(be.present).hover()
+    s(ITEM_CARD).should(be.present).hover()
 
 def is_item_removed():
-    s(__SUCCESS_MSG).should(have.text("has been removed from your Wish List."))
+    item_title = browser.driver.find_element(By.XPATH, ITEM_TITLE).text
+    s(ITEM_TITLE).should(have.text(item_title))
+
