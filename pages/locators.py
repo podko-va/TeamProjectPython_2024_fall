2b5eac1f51dcfeafefc742ee964df7885d1f6d36
unknown <lena.kilian91@gmail.com>
2024-05-08 12:15:01 +0800

    TC_002.001.001 | Radiant Tee product page > Visibility of product name, price and photo

--- pages/locators.py
+++ pages/locators.py
@@ -29,6 +29,10 @@ class ProductLocators:
     ARGUS_All_WEATHER_TANK_COLOR = '//*[@title="Argus All-Weather Tank"]/../..//*[@option-label="Gray"]'
     ARGUS_All_WEATHER_TANK_ADD_TO_CARD = '//*[@title="Argus All-Weather Tank"]/../..//*[@title="Add to Cart"]'
     MINI_BASKET_WINDOW = '[class="action showcart"]'
+    RADIANT_TEE_LINK = "//*[contains(text(), 'Radiant Tee')]"
+    RADIANT_TEE_IMG = 'img[alt="Radiant Tee"]'
+    RADIANT_TEE_TITLE = 'span[data-ui-id="page-title-wrapper"]'
+    RADIANT_TEE_PRICE = '#product-price-1556'
 
 
 class HomeLocators:
@@ -100,6 +104,7 @@ class WomenPageLocators:
     BOTTOMS_LINK = "//*[@id='ui-id-10']"
     BOTTOMS_TITLE = ".ui-corner-all ui-state-focus"
     DROPDOWN_BLOCK = "//*[@id='ui-id-2']/li[2]/ul"
+    TEES_LINK = '//*[@id="ui-id-13"]'
 
 
 class WhatsNewPageLocators:
--- pages/women_page.py
+++ pages/women_page.py
@@ -1,6 +1,6 @@
 from selene import browser
 from selene.support.shared.jquery_style import s
-from pages.locators import WomenPageLocators as WPL
+from pages.locators import WomenPageLocators as WPL, ProductLocators as PL
 from data.links import *
 
 
@@ -16,5 +16,18 @@ def click_dropdown_tops_link():
     s(WPL.TOPS_LINK).click()
 
 
+def move_to_tops_menu():
+    s(WPL.TOPS_LINK).hover()
+
+
 def click_dropdown_bottoms_link():
     s(WPL.BOTTOMS_LINK).click()
+
+
+def click_dropdown_tees():
+    s(WPL.TEES_LINK).click()
+
+
+def click_radiant_tee():
+    s(PL.RADIANT_TEE_LINK).click()
+
