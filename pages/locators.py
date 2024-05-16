668c033300e118fc330f455df30a08c2d6a28aca
ElenaBarteneva <149643166+ElenaBarteneva@users.noreply.github.com>
2024-05-16 14:59:30 +0800

    Update locators.py

--- pages/locators.py
+++ pages/locators.py
@@ -14,7 +14,7 @@ class SalePageLocators:
                                       'https://magento.softwaretestingboard.com/women/tops-women.html']
     BREADCRUMBS_LINKS_ON_PAGE_WOMEN_SALE = ['https://magento.softwaretestingboard.com/',
                                             'https://magento.softwaretestingboard.com/sale.html']
-    SALE_TAP = "//*[@id='ui-id-8']"
+    SALE_TAB = "//*[@id='ui-id-8']"
     BLOCK_PROMO_SALE_20_OFF_TITLE = "a[class='block-promo sale-20-off'] strong[class='title']"
     BLOCK_PROMO_SALE_20_OFF_INFO = "a[class='block-promo sale-20-off'] span[class='info']"
     BLOCK_PROMO_SALE_FREE_SHIPPING_TITLE = "a[class='block-promo sale-free-shipping'] strong[class='title']"
