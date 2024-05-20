0f764b4e115ad73320770ec687765990a7e87bf0
den_kho <20597299+denkho@users.noreply.github.com>
2024-05-20 23:54:52 +0300

    TC_002.001.012 no class inheritance

--- tests/test_product_page_argus_all_weather_tank.py
+++ tests/test_product_page_argus_all_weather_tank.py
@@ -1,8 +1,8 @@
 import allure
-from selene import browser
+from selene import browser, have, be
+from selene.support.shared.jquery_style import s
 
 import data.links as DL
-from pages.base_page import BasePage
 from pages.locators import ProductLocators as PL
 
 
@@ -11,8 +11,7 @@ class TestArgusAllWeatherTankPage:
     @allure.link('https://trello.com/c/dzrER7dp')
     @allure.title('TC_002.001.012 I Argus All-Weather Tank> Check whether the product name, its price and photo are displayed')
     def test_argus_allweather_tank_page_visibility_of_product_name_price_photo(self, login):
-        page = BasePage(browser=browser)
-        page.visit(DL.ARGUS_ALL_WEATHER_TANK_URL)
-        page.assert_text_of_element(PL.PRODUCT_TITLE, PL.ARGUS_ALL_WEATHER_TANK_PRODUCT_NAME_TEXT)
-        page.assert_present_of_element(PL.PRODUCT_PRICE_BASE)
-        page.assert_present_of_element(PL.PRODUCT_IMAGE_BASE)
+        browser.open(DL.ARGUS_ALL_WEATHER_TANK_URL)
+        s(PL.PRODUCT_TITLE).should(have.text(PL.ARGUS_ALL_WEATHER_TANK_PRODUCT_NAME_TEXT))
+        s(PL.PRODUCT_PRICE_BASE).should(be.present)
+        s(PL.PRODUCT_IMAGE_BASE).should(be.present)
