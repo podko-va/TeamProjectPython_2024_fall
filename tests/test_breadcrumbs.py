82d9011dd0d706eca88ec97d2c19df9cb647f9c2
Aleksandr <sasha345436@gmail.com>
2024-05-22 17:32:45 +0200

    TC_003.004.002 change code test_breadcrumbs.py

--- tests/test_breadcrumbs.py
+++ tests/test_breadcrumbs.py
@@ -5,7 +5,7 @@ from pages import breadcrumbs
 @allure.feature('Breadcrumbs')
 @allure.title('Verify that the breadcrumbs displayed and clickable')
 
-def test_breadcrumbs(chromium):
+def test_breadcrumbs_visible(chromium):
     breadcrumbs.open_page(chromium)
     breadcrumbs.select_item(chromium)
     breadcrumbs.is_clickable(chromium)
