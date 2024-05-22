853c83841761560ea8ea096cb20085a2b8d0e580
Aleksandr <sasha345436@gmail.com>
2024-05-22 22:00:51 +0200

    TC_003.004.002 change breadcrumbs.py test to selene

--- tests/test_breadcrumbs.py
+++ tests/test_breadcrumbs.py
@@ -4,9 +4,8 @@ from pages import breadcrumbs
 @allure.link('https://trello.com/c/s9FJicG5')
 @allure.feature('Breadcrumbs')
 @allure.title('Verify that the breadcrumbs displayed and clickable')
-
-def test_breadcrumbs(driver):
-    breadcrumbs.open_page(driver)
-    breadcrumbs.select_item(driver)
-    breadcrumbs.is_clickable(driver)
-    breadcrumbs.is_visible(driver)
+def test_breadcrumbs():
+    breadcrumbs.open_page()
+    breadcrumbs.nav_women_tops_jackets()
+    breadcrumbs.click_to_women_crumb()
+    breadcrumbs.is_visible()
