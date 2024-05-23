aa66a4aee4b646c828e23f1aee4ad1b7d1528211
Aleksandr <sasha345436@gmail.com>
2024-05-23 16:42:46 +0200

    TC_014.002.003 | Verify the function of pagination added a test and new functions to it

--- pages/limiter_item.py
+++ pages/limiter_item.py
@@ -1,4 +1,4 @@
-from selene import browser, have
+from selene import browser, have, be
 from selene.support.shared.jquery_style import s
 
 url = 'https://magento.softwaretestingboard.com/customer/account/login'
@@ -7,8 +7,11 @@ user_email = s("div.login-container #email")
 user_password = s("div.login-container #pass")
 sign_in_button = s("div.login-container #send2")
 limiter = '//*[@id="limiter"]'
-limit = '//*[@id="limiter"]/option[2]'
-items = '//*[@id="maincontent"]/div[2]/div[1]/div[3]/div/p/span'
+limit_20 = '//*[@id="limiter"]/option[2]'
+limit_10 = '//*[@id="limiter"]/option[1]'
+toolbar_number = '//*[@class="toolbar-number"]'
+next_page = '//*[@class="action  next"]'
+item = '//*[@class="product-item"][1]'
 
 user = "alexx.shigaev@gmail.com"
 password = "B2a6ig_a9Hb3cz@"
@@ -20,9 +23,21 @@ def preconditions():
     sign_in_button.click()
     browser.open(wish_list)
 
-def change_limiter():
+def change_limit_to_20():
     s(limiter).click()
-    s(limit).click()
+    s(limit_20).click()
 
-def change_verification():
-    s(items).should(have.text('11 Item(s)'))
+def change_limit_to_10():
+    s(limiter).click()
+    s(limit_10).click()
+
+def items_number():
+    s(toolbar_number).should(have.text('11 Item(s)'))
+
+def items_amount():
+    s(toolbar_number).should(have.text('Items 11 to 11 of 11 total'))
+
+def item_page_item_next():
+    s(next_page).click()
+    s(item).should(be.visible)
+
