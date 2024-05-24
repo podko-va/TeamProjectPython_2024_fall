e98cd3400a32f6d4b37fa54be74ed1613333ac55
Andrey Zh. <AndZhelBack@gmail.com>
2024-05-24 22:24:00 +0400

    TC_016.001.008 | Advanced Search > Products found match the search criteria

--- tests/test_advanced_search.py
+++ tests/test_advanced_search.py
@@ -4,9 +4,10 @@ from pages import advanced_search
 
 @allure.link('https://trello.com/c/kn91s1De')
 @allure.feature("Advanced Search")
+@allure.title('Empty input fields')
 def test_input_fields_are_empty():
     advanced_search.open()
-    advanced_search.click_button()
+    advanced_search.click_search_button()
     advanced_search.message_text()
 
 @allure.link('https://trello.com/c/X8H9lLun/')
@@ -15,7 +16,7 @@ def test_input_fields_are_empty():
 def test_incorrect_price_range():
     advanced_search.open()
     advanced_search.fill_wrong_price_range()
-    advanced_search.click_button()
+    advanced_search.click_search_button()
     advanced_search.price_range_error_message()
 
 @allure.link('https://trello.com/c/HCQ997pb/')
@@ -24,5 +25,14 @@ def test_incorrect_price_range():
 def test_prohibited_characters_in_price():
     advanced_search.open()
     advanced_search.fill_prohibited_characters_in_price()
-    advanced_search.click_button()
+    advanced_search.click_search_button()
     advanced_search.invalid_number_price_error_message()
+
+@allure.link('https://trello.com/c/fpWChrDR/')
+@allure.feature('Advanced Search')
+@allure.title('Products found match the search criteria')
+def test_correct_results_of_the_advanced_search():
+    advanced_search.open()
+    advanced_search.search_by_product_name()
+    advanced_search.click_search_button()
+    advanced_search.check_search_result()
