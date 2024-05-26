a619ad5317ff9ca081a9d38dfd6ec2c26ed1d493
Andrey Zh. <AndZhelBack@gmail.com>
2024-05-26 17:46:47 +0400

    TC_016.001.004 | Advanced Search > Advanced search is carried out in all fields

--- tests/test_advanced_search.py
+++ tests/test_advanced_search.py
@@ -10,7 +10,6 @@ def test_input_fields_are_empty():
     advanced_search.click_search_button()
     advanced_search.message_text()
 
-
 @allure.link('https://trello.com/c/X8H9lLun/')
 @allure.feature('Advanced Search')
 @allure.title('Entering an incorrect price range')
@@ -19,7 +18,6 @@ def test_incorrect_price_range():
     advanced_search.fill_wrong_price_range()
     advanced_search.click_search_button()
     advanced_search.price_range_error_message()
-
 
 @allure.link('https://trello.com/c/HCQ997pb/')
 @allure.feature('Advanced Search')
@@ -38,3 +36,12 @@ def test_correct_results_of_the_advanced_search():
     advanced_search.search_by_product_name()
     advanced_search.click_search_button()
     advanced_search.check_search_result()
+
+@allure.link('https://trello.com/c/Pz12v6Hs/')
+@allure.feature('Advanced Search')
+@allure.title('Filling out all input fields')
+def test_all_input_fields_are_filled_in():
+    advanced_search.open()
+    advanced_search.fill_in_all_input_fields('Jacket', 'MJ03', 'Adjustable hood.', 'Adjustable hood.', '40', '50')
+    advanced_search.click_search_button()
+    advanced_search.check_full_search_results('Jacket', 'MJ03', 'Adjustable hood.', 40, 50)
