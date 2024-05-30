b4317ee506df4c73e5afcafc39afcf2ddf4708af
ElenaBarteneva <149643166+ElenaBarteneva@users.noreply.github.com>
2024-05-30 14:53:26 +0800

    Update test_check_quantity_items_are_displayed_in_compare_products_section.py

--- tests/test_check_quantity_items_are_displayed_in_compare_products_section.py
+++ tests/test_check_quantity_items_are_displayed_in_compare_products_section.py
@@ -3,13 +3,13 @@ import allure
 import pytest
 
 
-# @pytest.mark.skip
+@pytest.mark.skip
 @allure.title("check value of selected items to compare products section when compare product section is empty")
 def test_check_quantity_value_in_empty_compare_product_section():
     compare_items.compare_items_values_in_empty_compare_items_section()
 
 
-# @pytest.mark.skip
+@pytest.mark.skip
 @allure.title("check selected items from compare products section are deleted after clicking on clear all btn")
 def test_check_compare_product_item_section_is_empty_after_click_on_clear_all_btn(browser_management):
     compare_items.compare_items_values_in_empty_compare_items_section()
